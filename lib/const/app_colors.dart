96d0d27cbf75a34fda17dd896dae9acf9e10839b
Sameer Kumar <sameerkumar.tman@gmail.com>
2022-05-23 18:26:58 +0530

    [App] Create file to store colors

---
+++ lib/const/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CommonColor {
+  static const baseColor = Color(0xff6c63ff);
+  static const baseColor2 = Color(0xff2f2e41);
+  static const appBarColor = Color.fromARGB(255, 180, 180, 180);
+  static const lightTextColor = Color.fromARGB(255, 97, 96, 96);
+  static const errorColor = Colors.red;
+}
