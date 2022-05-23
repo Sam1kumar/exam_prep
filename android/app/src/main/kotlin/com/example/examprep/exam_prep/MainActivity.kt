c44414e4fdf9ff44684e17eff832d0e77cc1db34
Sameer Kumar <sameerkumar.tman@gmail.com>
2022-05-23 18:26:23 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/examprep/exam_prep/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.examprep.exam_prep
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_dev.dart
@@ -0,0 +1,6 @@
+import 'package:exam_prep/app/flavor.dart';
+import 'package:exam_prep/app/main_common.dart';
+
+void main() {
+  mainCommon(env: Environment.dev());
+}
